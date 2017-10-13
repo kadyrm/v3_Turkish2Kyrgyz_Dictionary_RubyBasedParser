304e87d9efbfee7daf23243525733d5168c0fce3
lab112-pc09 <lab112-pc09@manas.com>
2017-10-13 16:56:25 -0400

    Successfully ran first nokogiri parser using io file

--- phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
+++ phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
@@ -1,8 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
 
-html_data = File.read('index.html')
+html_data = File.read('../input/Cumakunova_tr_kg[901-1000].htm')
 
 nokogiri_object = Nokogiri::HTML(html_data)
 
-x_element = nokogiri_object.xpath("/html/body/div[340]/p[16]/span[15]")
+x_element = nokogiri_object.xpath("/html/body/div/p/span[@style=\"color:red\"]")
+File.write('../output/write_html_to.html', nokogiri_object.to_html(encoding: 'UTF-8'))
+=begin
+# code snippet from: https://stackoverflow.com/questions/24086304/nokogiri-write-html-to-oddness
+# No1:
+#File.write('write_html_to.html', doc.to_html(encoding: 'UTF-8'))
+# No2:
+#html_data.write_xhtml_to(File.new('write_xhtml_to.html', 'w'), :encoding => 'UTF-8')
+# No3:
+# modules.each do |url, klass|
+=end
