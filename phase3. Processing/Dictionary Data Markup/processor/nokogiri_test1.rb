baf1a49106324a39b33977e54dcdf445ed80bf6b
lab112-pc09 <lab112-pc09@manas.com>
2017-10-16 17:03:13 -0400

    Defined output encoding problem
    No Turkish and Kyrgyz letters rendered correctly or text content is distorted at all

--- phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
+++ phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
@@ -6,7 +6,16 @@ html_data = File.read('../input/Cumakunova_tr_kg[901-1000].htm')
 nokogiri_object = Nokogiri::HTML(html_data)
 
 x_element = nokogiri_object.xpath("/html/body/div/p/span[@style=\"color:red\"]")
-File.write('../output/write_html_to.html', nokogiri_object.to_html(encoding: 'UTF-8'))
+
+# output below doesn't preserve turkish and kyrgyz specific letter
+nokogiri_object.write_xhtml_to(File.new('../output/write_html_to.html', 'w'), :encoding => 'UTF-8')
+
+# output below doesn't preserve content text at all
+#File.write('../output/write_html_to.html', nokogiri_object.to_html(encoding: 'UTF-8'))
+
+
+
+
 =begin
 # code snippet from: https://stackoverflow.com/questions/24086304/nokogiri-write-html-to-oddness
 # No1:
