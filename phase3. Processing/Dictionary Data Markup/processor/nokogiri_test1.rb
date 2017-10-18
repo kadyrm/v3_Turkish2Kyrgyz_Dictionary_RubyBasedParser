17e1360f6b73e63f265ab03349be709798221804
lab112-pc09 <lab112-pc09@manas.com>
2017-10-18 16:27:44 -0400

    Successfully accessed margin-top value of each <p> element

--- phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
+++ phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'nokogiri-styles'
 
 def DelimitLines(oDOM)
         x_element = oDOM.xpath("/html/body/div/p/span[@style=\"color:red\"]")
@@ -23,13 +24,41 @@ def MarkupDictData(oDOM)
 end
 #==========================================================================
 def MarkupColumns(o_div)
-        puts o_div.to_s()
+        #puts o_div.to_s()
 	p_elems = o_div.xpath("./p")
 	p_elems.each() do |node|
-                puts p_elems.index(node) +1
-                #puts node.to_html()
-		puts node.attribute("style")
+                puts "element: " + (p_elems.index(node) +1).to_s()                  
+		#puts node["style"]
+		puts "margin-top:\t" + node.styles['margin-top'].to_s()
+		if node.styles['margin-top']== "4.1pt"
+			puts "++++++++++++++++++++COLUMN BREAK HAS BEEN FOUND++++++++++++++++++++++++++++"		
+		end
 	end
+
+end
+def ParsingOfAttrValues(_oDOM)
+
+	node = _oDOM.xpath("/html/body/div[3]/p[0]")
+	# ...
+
+	# Get styles
+	node['style']         # => 'width: 400px; color: blue'
+	node.styles['width']  # => '400px'
+	node.styles['color']  # => 'blue'
+
+	# Update styles
+	style = node.styles
+	style['width']  = '500px'
+	style['height'] = '300px'
+	style['color']  = nil
+	node.styles = style
+	node['style']         # => 'width: 500xp; height: 300px'
+
+	# Modify classes
+	node['class']         # => 'foo bar'
+	node.classes          # => ['foo', 'bar']
+	node.classes = ['foo']
+	node['class']         # => 'foo'
 
 end
 #==========================================================================
@@ -42,6 +71,8 @@ oDOM = Nokogiri::HTML(html_data)
 
 div_set = oDOM.xpath("/html/body/div")
 MarkupColumns(div_set[3])
+puts "\n NEXT PAGE GOES HERE========================================="
+MarkupColumns(div_set[7])
 
 # output below doesn't preserve turkish and kyrgyz specific letter
 oDOM.write_xhtml_to(File.new('../output/write_html_to.html', 'w'), :encoding => 'UTF-8')
