da7dafabe3d56666d60f4c207daae1973d2dcdd3
lab112-pc09 <lab112-pc09@manas.com>
2017-11-10 21:41:03 -0500

    Added a file to track changes in a single element under process

--- phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
+++ phase3. Processing/Dictionary Data Markup/processor/nokogiri_test1.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 #*******************************************************************
 def remove_blank_tags(_enter_point, _tag_name)
-	xpath_query  = ".//" + _tag_name+"[not(*)][not(normalize-space())]"
+	xpath_query  = ".//" + _tag_name+"[not(node())]"
 	node_set = _enter_point.xpath(xpath_query)
         node_set.each() do |node|
 		node.remove
@@ -12,6 +12,8 @@ end
 def test_remove_blank_tags(_oDOM)
         entrance = _oDOM.xpath("/html/body/div[4]")
 	remove_blank_tags(entrance[0], "span")
+	entrance[0].write_xhtml_to(File.new('./compare/after.html', 'w'), :encoding => 'UTF-8')
+
 end
 #-------------------------------------------------------------------
 def merge_paired_tags(_enter_point, _tag_name)
